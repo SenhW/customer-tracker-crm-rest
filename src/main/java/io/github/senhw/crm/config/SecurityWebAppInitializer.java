f60011f3f7b0ab4381062b3f2499e8ce4995bdd9
SenhW <senhwang3@gmail.com>
2019-05-18 16:43:40 -0400

    Securing REST API endpoints with Spring Security
    Restrict certain user roles from accessing certain endpoints

---
+++ src/main/java/io/github/senhw/crm/config/SecurityWebAppInitializer.java
@@ -0,0 +1,8 @@
+package io.github.senhw.crm.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+// This class enables the Spring Security filters for the web application.
+public class SecurityWebAppInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
